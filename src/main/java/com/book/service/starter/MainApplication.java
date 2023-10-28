01852543f5e45459da706af9f7be350eb7faaa68
akuma189 <raj_adlagattu@optum.com>
2023-10-28 21:02:36 +0530

    adding component scan

--- src/main/java/com/book/service/starter/MainApplication.java
+++ src/main/java/com/book/service/starter/MainApplication.java
@@ -8,9 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan("com.employee")
-@EntityScan("com.employee.entity")
-@EnableJpaRepositories("com.employee.repository")
+@ComponentScan("com.book.service")
+@EntityScan("com.book.service.entity")
+@EnableJpaRepositories("com.book.service.repository")
 public class MainApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 	      SpringApplication.run(MainApplication.class, args);
